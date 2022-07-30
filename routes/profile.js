21352e3461d859c21d8f63ca39d803c0951a7242
mamrudien <muh.amrudien@gmail.com>
2022-07-30 14:00:04 +0700

    route profile/ on backend no longer required

--- routes/profile.js
+++ routes/profile.js
@@ -3,13 +3,13 @@ const { uploadImage } = require('../controllers/imageUpload')
 const { getProfile, updateProfile, getBasicProfile } = require('../controllers/profile')
 
 //Get basic profile info (email, name, photo, etc)
-router.get('/basic', getBasicProfile)
+//router.get('/basic', getBasicProfile)
 
 //Get complete profile data for edit profile page
-router.get('/', getProfile)
+//router.get('/', getProfile)
 
 //Update profile data from edit profile page
-router.post('/', uploadImage([{ name: 'photo', maxCount: 1 }, { name: 'paymentReceiptImg', maxCount: 1 } ]), updateProfile)
+router.post('/upload', uploadImage([{ name: 'photo', maxCount: 1 }, { name: 'paymentReceiptImg', maxCount: 1 } ]), updateProfile)
 
 // router.post('/test', uploadImage([{ name: 'photo', maxCount: 1 }, { name: 'paymentReceiptImg', maxCount: 1 } ]), (req, res) => {
 //   return res.status(200).json({ data: res.locals.uploadUrl })
